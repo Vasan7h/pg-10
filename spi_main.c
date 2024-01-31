dfec0d17e130cf72d00afb9e7642a90bf133d205
Vasan7h <vasanthparthasarathi@gmail.com>
2024-01-31 12:03:39 +0530

    changed wrt unit2

--- spi_main.c
+++ spi_main.c
@@ -16,7 +16,6 @@
 #include "analog.h"
 // initial overshoot applied
 // osicllation fixed
-// power val >600 range in adc out set to 0 W
 extern int8_t Temperature_i2c_fd;
 int fault_flag = 0;
 int stable_dac;
@@ -24,8 +23,6 @@ double tol = 0;
 int stable_loop_init_flag = 0;
 int err_sign;       // calculates err is +ve or -ve
 int overshoot_flag; // to acheive shoot initially
-uint8_t making_zero_flag = 0;
-uint8_t pid_entering_after_Zero = 0;
 int interlock_flag = 0, ref_fault_flag = 0, overheat_warn_flag = 0;
 double forward_power = 0;
 double prev_forward_power = 0;
@@ -39,8 +36,8 @@ int adc_nc_flag;                          // flag to check 4 counts in stable lo
 double osci_fix = 0;                      // for stabilizing the pow_set_ptput
 int pow_set_pt;                           // set point (RF input)
 double final_forward_power_pps_dac_value; // forw power within PID control
-uint8_t final_forward_power;               // forw power inside PID when stable o/p
-uint8_t Prev_final_forward_power = 0;      // to store previous forward power
+double final_forward_power;               // forw power inside PID when stable o/p
+double Prev_final_forward_power = 0;      // to store previous forward power
 double t_pps_dac_value = 0;               // 10% of max DAC value (actual 255) , limited for 600W
 double I_pe = 0;                          // previous error_cal
 double D_pe = 0;                          // previous error_cal
@@ -56,12 +53,13 @@ double I_control;                         // integral term
 double D_control;                         // differential term
 double error_cal;                         // error_cal
 int power_pps_dac_value;
-double err_percent;  // finding percentage of error
-double ctrl_percent; // percentage of val to be reduced from initial overshoot DAC value
 float a_dc;
+int count=1; //SOL 3
 int16_t pps_dac_value_PID;
+uint8_t dac_at_err_50_to_100 
 uint8_t pps_dac_value; // DAC value
 int pps_dac_value_f;
+int stay_max_DAC_flag = 0;
 char set_flag_rev = 0, set_flag_for = 0;
 uint8_t matched_condition = 0;
 int16_t over_heat_detect = 0;
@@ -74,7 +72,7 @@ int DAC_forw_ref_pow_spi_fd = 0;
 int interlock = 0;
 int rf_control = 0;
 uint16_t data = 0;
-uint8_t final_reflected_power = 0;
+double final_reflected_power = 0;
 double reflected_power = 0;
 int8_t condition_check = 0;
 double load_power = 0;
@@ -90,10 +88,7 @@ int spi_main()
         power_set_point();
         if (fault_flag | interlock_flag | (!pow_set_pt))
         {
-            printf("RF OFF\n");
-            power_set_point();
-            printf("Setting %d Watt\n", pow_set_pt);
-
+            printf("Setting 0 Watt\n");
             // analog_main();
             pps_dac_value = 50;
             Volt_cur_forw_ref_load_Set(DAC_spi_fd, DAC_DA2_SEL_CMD, spi1_trx, pps_dac_value); // programmable power supply dac set point
@@ -113,9 +108,7 @@ int spi_main()
             overshoot_flag = 1;
             error_cal = 0;
             I_pe = error_cal;
-            pid_entering_after_Zero = 0;
             D_pe = error_cal;
-            making_zero_flag = 0;
             printf("I AM ENTERING FOR FIRST TIME\n");
             while ((((!interlock_flag) && (!fault_flag) && (pow_set_pt > 0))))
             {
@@ -124,32 +117,122 @@ int spi_main()
                     rf_within_regulation(); // LED
                 }
                 power_set_point();
+                manual_absl_func();
                 printf("Setting %d Watt\n", pow_set_pt);
-                printf("forw pow in check:    %d W \n", final_forward_power);
+                printf("forw pow in check:    %lf W \n", final_forward_power);
                 error_cal = (pow_set_pt - final_forward_power);
                 printf("Err: %lf\n", error_cal);
 
                 printf("ncflag %d\n", stable_loop_init_flag);
-
+                if (stable_loop_init_flag)
+                {
+                    prev_pps_dac_value = 0;
+                }
+                if ((error_cal < 100) && (error_cal > 50)) 
+                {
+                    stay_max_DAC_flag = 1;
+                    count=0;
+                }
                 if (((abs(error_cal)) > (0.02 * pow_set_pt)) && (!ref_fault_flag))
                 {
                     printf("abs_error %d\n", (abs(error_cal)));
                     if (overshoot_flag)
                     {
-                        pps_dac_value = (0.24 * (pow_set_pt + 100)) + 74;
+                        pps_dac_value = (0.24 * (pow_set_pt + 100)) + 85; // dac for overshoot initially
                         overshoot_flag = 0;
+                    }
+                    if (stay_max_DAC_flag)
+                    {
+                        pps_dac_value = prev_pps_dac_value; //SOL 1
+                        printf("IN SOL 1 for %d counts\n",count);
+                        //pps_dac_value =0 ; //SOL 2
+                        //pps_dac_value=0;   //SOL 3
+                        if(count >= 2)
+                        {
+                        stay_max_DAC_flag = 0;
+                        //pps_dac_value =dac_at_err_50_to_100;
+                        }
+                        count++;
                     }
                     else
                     {
-                        // logic to calculate DAC value
-                        err_percent = ((error_cal / pow_set_pt) * 100);
-                        printf("err_perc %lf\n", err_percent);
-                        ctrl_percent = err_percent / (err_percent + pow_set_pt);
-                        printf("perc_to_reduce_from_shoot_DAC %lf\n", ctrl_percent);
-                        pps_dac_value = prev_pps_dac_value + (prev_pps_dac_value * ctrl_percent);
-
-                        // storing previous error
-                        // pps_dac_value_PID = (int16_t)(P_control + I_control + D_control); // casting double to int
+                        if (offset_flag)
+                        {
+                            prev_pps_dac_value = 0; // prev dac = 0 while entering pid loop for first time
+                        }
+                        // to choose max dac val pow_set_pt of 255
+                        if ((abs(error_cal)) > (FIFTY_PERCENT * pow_set_pt)) // err > 50% of set_point
+                        {
+                            t_pps_dac_value = (TWENTY_PERCENT * 240); // 20% of max dac val
+                            offset_flag = 1;
+                        }
+                        else
+                        {
+                            t_pps_dac_value = (TEN_PERCENT * 240); // 10% of max dac val
+                        }
+                        if ((abs(error_cal)) > (TWENTY_PERCENT * pow_set_pt)) // err > 20% set value
+                        {
+                            kp = 0.0065; // 0.65% gain change
+                        }
+                        else
+                        {
+                            kp = 0.002;
+                        }
+                        if (offset_flag)
+                        {
+                            if (abs(error_cal) > (EIGHTY_PERCENT * pow_set_pt))
+                            {
+                                if ((pow_set_pt > 350) && (pow_set_pt < 490))
+                                {
+                                    ki = 0.0024789;
+                                    kd = 0.0025;
+                                }
+                                else if ((pow_set_pt >= 220) && (pow_set_pt < 350))
+                                {
+                                    ki = 0.007;
+                                    kd = 0.0013;
+                                }
+                                else if ((pow_set_pt >= 120) && (pow_set_pt < 220))
+                                {
+                                    ki = 0.008;
+                                    kd = 0.0035;
+                                }
+                                else if (pow_set_pt < 120)
+                                {
+                                    kp = 0.009;
+                                    ki = 0.0031;
+                                    kd = 0.005;
+                                }
+                                else
+                                {
+                                    ki = 0.002;
+                                    kd = 0.00125;
+                                }
+                            }
+                            else
+                            {
+                                ki = 0.001;
+                                kd = 0.0013;
+                            }
+                        }
+                        else
+                        {
+                            kd = 0.002;
+                            ki = 0.002;
+                        }
+                        P_control = kp * t_pps_dac_value * error_cal; // 0.1 % gain
+                        // printf("kp : %lf\n", P_control);
+                        Integration_cal = (error_cal + I_pe) / 2; // integral value
+                        // printf("integral : %lf",Integration_cal);
+                        I_control = ki * Integration_cal * t_pps_dac_value; // 0.1 % gain
+                        // printf("ki : %lf\n", I_control);
+                        Differentiation_cal = (error_cal - D_pe);
+                        // printf("derivative : %lf",Differentiation_cal);     // derivative
+                        D_control = kd * Differentiation_cal * t_pps_dac_value; // 0.1 % gain
+                        // printf("kd: %lf\n", D_control);
+                        I_pe = error_cal;                                                 // storing previous error
+                        D_pe = error_cal;                                                 // storing previous error
+                        pps_dac_value_PID = (int16_t)(P_control + I_control + D_control); // casting double to int
                         printf(" PPS PID value: %d\n", pps_dac_value_PID);
                         err_sign = ((((int32_t)(abs(error_cal))) / ((int32_t)error_cal)));
                         if ((abs(error_cal) > (0.03 * pow_set_pt)) && (!offset_flag))
@@ -162,9 +245,8 @@ int spi_main()
                             toler_set = 1; // changed from 1 to 2
                             condition_check = (toler_set)*err_sign;
                         }
-
                         // adding offset to acheive desired power
-                        pps_dac_value = pps_dac_value + condition_check;
+                        pps_dac_value_PID = pps_dac_value_PID + condition_check + prev_pps_dac_value;
                         pid_overflow_fix();
                         // in order to create an overshoot
                     }
@@ -172,22 +254,34 @@ int spi_main()
                     // updating dac for pps
                     Volt_cur_forw_ref_load_Set(DAC_spi_fd, DAC_DA2_SEL_CMD, spi1_trx, pps_dac_value);
                     prev_pps_dac_value = pps_dac_value;
+                    //dac_at_err_50_to_100 =pps_dac_value;  //SOL 3
                     //  reading for_pow
                     usleep(20000);                     // 20 ms delay
                     usleep(round(pow_set_pt * 33.33)); // Delay calculation as per setpoint
-
                     Read_forward_power();
                     Read_reflected_power();
+                    offset_flag = 0;
+                    overshoot_flag = 0;
+                    power_set_point();
+                    error_cal = (pow_set_pt - final_forward_power);
+
+                    // int abserr=(int)(abs(error_cal));
+                    // printf("abserr %lf\n",abserr);
+                    manual_absl_func();
                     printf("Err: %lf\n", error_cal);
-                    if ((int)error_cal <= (int)(0.02 * pow_set_pt))  //condn to start filtering
+                    if ((int)error_cal <= (int)(0.02 * pow_set_pt))
                     {
                         printf("abs_error %d\n", (abs(error_cal)));
                         stable_loop_init_flag = 1;
                     }
                 }
-
                 else
                 {
+                    // forw dac update
+
+                    // analog_main();                      // reading voltage from pps
+                    //  forw_pow_calculation
+
                     error_cal = (pow_set_pt - final_forward_power);
                     printf("Err in stable PID: %lf\n", error_cal);
                     osci_fix = ((((int32_t)(abs(error_cal))) / ((int32_t)error_cal))); // err sign
@@ -197,17 +291,15 @@ int spi_main()
                         if ((abs(error_cal) > 2) && (!ref_fault_flag))
                         { // err > 2 W
                             printf("offset   %lf\n", osci_fix);
-                            printf("dac val %d \n", pps_dac_value);
                             pps_dac_value = pps_dac_value + osci_fix; // adding offset to acheive desired o/p
                                                                       // prev_pps_dac_value = pps_dac_value; // storing dac val
                         }
                     }
                     else
                     {
-                        if (((abs(error_cal)) >= tol) && (!ref_fault_flag))
+                        if (((fabs(error_cal)) >= tol) && (!ref_fault_flag))
                         {
                             printf("offset   %lf\n", osci_fix);
-                            printf("dac val %d \n", pps_dac_value);
                             pps_dac_value = pps_dac_value + osci_fix; // adding offset to acheive desired o/p
                             // prev_pps_dac_value = pps_dac_value; // storing dac val
                         }
@@ -220,19 +312,19 @@ int spi_main()
                     Read_forward_power();
                     Read_reflected_power();
                     // calculating load power
-
+                    // IIR filter
                     if (prev_pps_dac_value == pps_dac_value)
                     {
                         final_forward_power = (Prev_final_forward_power + final_forward_power) / 2;
                         printf("filtering noise\n");
                     }
-                    printf("forw pow after noise fixing:    %d W \n", final_forward_power);
+                    printf("forw pow after noise fixing:    %lf W \n", final_forward_power);
                     load_power = final_forward_power - final_reflected_power;
                     // DB25 updation
 
                     dac_set(DAC_forw_ref_pow_spi_fd, final_forward_power, final_reflected_power, load_power);
                     Prev_final_forward_power = final_forward_power;
-                    printf("forw pow in after dac:    %d W \n", final_forward_power);
+                    printf("forw pow in after dac:    %lf W \n", final_forward_power);
                     prev_pps_dac_value = pps_dac_value; // storing dac val
                     // stabilizing for +/- 3 watts
                     // Read_forward_power();
@@ -241,10 +333,10 @@ int spi_main()
                     stable_loop_init_flag = 0;
                     // printf("forw pow in stable:    %lf W \n", final_forward_power);
                 }
+                printf("\n");
                 rf_on_off_interlock_functioning();
                 temp_functioning();
                 ref_power_condition_check();
-                printf("\n");
             }
         }
         printf("\n");
@@ -257,7 +349,7 @@ void Read_forward_power(void)
     forward_power = avg_value(EMB_spi_fd, FOR_POW);
     printf("raw forw %lf\n", forward_power);
     final_forward_power = calibrated_result(forward_power, FOR_POW);
-    printf("forw pow :    %d W \n", final_forward_power);
+    printf("forw pow :    %lf W \n", final_forward_power);
 }
 
 void Read_reflected_power(void)
@@ -265,7 +357,7 @@ void Read_reflected_power(void)
     reflected_power = avg_value(EMB_spi_fd, REF_POW);
     printf("raw ref %lf\n", reflected_power);
     final_reflected_power = calibrated_result(reflected_power, REF_POW);
-    printf("Ref pow:     %d W\n", final_reflected_power);
+    printf("Ref pow:     %lf W\n", final_reflected_power);
     printf("\n");
 }
 
@@ -296,20 +388,36 @@ void ref_power_condition_check(void)
 
 void pid_overflow_fix(void)
 {
-    if (pps_dac_value > 255)
+    if (pps_dac_value_PID > 255)
     {
-        pps_dac_value = 255;
+        pps_dac_value_PID = 255;
     }
-    // pps_dac_value = (uint8_t)pps_dac_value_PID;
+    pps_dac_value = (uint8_t)pps_dac_value_PID;
 }
 
 void power_set_point(void)
 {
     data = SPI1SS1_ADC_readRegister(SPI1SS1_ADC_MCU_fd, spi3_trx);
-    pow_set_pt = (int)round((data * 2.986165566) + 1.218628746);
+    printf("raw_adc %d\n", data);
+    a_dc = (float)data * 0.05;
+    printf("adc_manipulated %lf\n", a_dc);
+    if (a_dc > 7)
+    {
+        a_dc = a_dc - 0.05;
+    }
+    if (pow_set_pt > 298)
+    {
+        pow_set_pt = (a_dc * 60) + 1;
+    }
+    // printf("set_pt %d\n",pow_set_pt);
+    //  pow_set_pt=pow_set_pt;
     if (pow_set_pt >= 600) // limited for 600 W
     {
         pow_set_pt = 600;
+    }
+    else
+    {
+        pow_set_pt = (a_dc * 60);
     }
 }
 
@@ -368,6 +476,18 @@ void rf_on_off_interlock_functioning(void)
         set_gpio(RF_STATUS, OFF);
         interlock_flag = 1;
         system_ready();
+    }
+}
+
+void manual_absl_func(void)
+{
+    if (error_cal < 0) // check for negative error
+    {
+        error_cal = -1 * error_cal; // manually creating absolute function
+    }
+    else
+    {
+        error_cal=error_cal;
     }
 }
 
